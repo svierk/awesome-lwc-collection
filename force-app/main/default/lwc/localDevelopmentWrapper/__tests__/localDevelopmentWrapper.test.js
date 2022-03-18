b567ddcfe5857216cc1bf4f71a4555d2473de23d
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-03-18 17:08:32 +0100

    feat(lwc): add local development wrapper component

---
+++ force-app/main/default/lwc/localDevelopmentWrapper/__tests__/localDevelopmentWrapper.test.js
@@ -0,0 +1,23 @@
+import LocalDevelopmentWrapper from 'c/localDevelopmentWrapper';
+import { createElement } from 'lwc';
+
+describe('c-local-development-wrapper', () => {
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it('should create', () => {
+    // given
+    const element = createElement('c-local-development-wrapper', {
+      is: LocalDevelopmentWrapper
+    });
+    document.body.appendChild(element);
+
+    // then
+    return Promise.resolve().then(() => {
+      expect(element).toBeTruthy();
+    });
+  });
+});
---
+++ force-app/main/default/lwc/localDevelopmentWrapper/localDevelopmentWrapper.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * A simple wrapper component to locally test input parameters of other components.
+ * @alias LocalDevelopmentWrapper
+ * @extends LightningElement
+ * @hideconstructor
+ */
+export default class LocalDevelopmentWrapper extends LightningElement {}
