b13e342ae86ad4359e014afef553ec86335b3896
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-03-18 17:02:11 +0100

    docs(lwc): add missing docs for hello world sample component

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,5 +1,18 @@
 import { api, LightningElement } from 'lwc';
 
+/**
+ * An example LWC that adds a classic greeting to any page.
+ * @alias HelloWorld
+ * @extends LightningElement
+ * @hideconstructor
+ *
+ * @example
+ * <c-hello-world name="World"></c-hello-world>
+ */
 export default class HelloWorld extends LightningElement {
+  /**
+   * Enter the name of the person to greet.
+   * @type {string}
+   */
   @api name;
 }
