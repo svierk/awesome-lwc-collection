6be57756e5c47551e1c2441b93e9ad29d49979cd
Sebastiano Vierk <s.vierk@hotmail.de>
2022-04-20 08:32:54 +0200

    test(lwc): improve existing jest unit tests

--- force-app/main/default/lwc/multiSelectCombobox/__tests__/multiSelectCombobox.test.js
+++ force-app/main/default/lwc/multiSelectCombobox/__tests__/multiSelectCombobox.test.js
@@ -16,6 +16,7 @@ describe('c-multi-select-combobox', () => {
     while (document.body.firstChild) {
       document.body.removeChild(document.body.firstChild);
     }
+    jest.clearAllMocks();
   });
 
   it('should show combobox label', () => {
@@ -46,13 +47,13 @@ describe('c-multi-select-combobox', () => {
 
     // when
     document.body.appendChild(element);
+    const input = element.shadowRoot.querySelector('.multi-select-combobox__input');
+    input.click();
+    const itemList = element.shadowRoot.querySelector('ul[class="slds-listbox slds-listbox_vertical"]');
 
     // then
     return Promise.resolve().then(() => {
-      setTimeout(() => {
-        const childs = element.shadowRoot.querySelectorAll('c-multi-select-combobox-item');
-        expect(childs.length).toBe(4);
-      }, 0);
+      expect(itemList.childNodes.length).toBe(mockData.options.length);
     });
   });
 
@@ -108,7 +109,7 @@ describe('c-multi-select-combobox', () => {
       });
 
       // then
-      expect(mockChangeHandler).toHaveBeenCalledTimes(4);
+      expect(mockChangeHandler).toHaveBeenCalledTimes(mockData.options.length);
     });
   });
 
