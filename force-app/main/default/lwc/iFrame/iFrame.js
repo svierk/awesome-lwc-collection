09e5a8e17079340512018136cdc181e4894eb9d3
Sebastiano Vierk <s.vierk@hotmail.de>
2022-07-29 12:48:47 +0200

    feat(lwc): add iframe component

---
+++ force-app/main/default/lwc/iFrame/iFrame.js
@@ -0,0 +1,53 @@
+import { api, LightningElement } from 'lwc';
+
+/**
+ * A custom iFrame component with different configuration options.
+ * @alias IFrame
+ * @extends LightningElement
+ * @hideconstructor
+ *
+ * @example
+ * <c-i-frame
+ *   url="https://example.com/"
+ *   height="500px"
+ *   width="100%"
+ *   referrerpolicy="no-referrer"
+ *   sandbox=""
+ * ></c-i-frame>
+ */
+export default class IFrame extends LightningElement {
+  /**
+   * Specifies the height of the iframe. Default height is 500 pixels.
+   * @type {string}
+   * @default ''
+   */
+  @api height = '500px';
+
+  /**
+   * Specifies which referrer information to send when fetching the iframe.
+   * @type {string}
+   * @default ''
+   */
+  @api referrerPolicy = 'no-referrer';
+
+  /**
+   * Enables an extra set of restrictions for the content in an iframe.
+   * @type {string}
+   * @default ''
+   */
+  @api sandbox = '';
+
+  /**
+   * Specifies the address of the document to embed in the iframe.
+   * @type {string}
+   * @default ''
+   */
+  @api url = '';
+
+  /**
+   * Specifies the width of an iframe. Default width is 100 percent.
+   * @type {string}
+   * @default ''
+   */
+  @api width = '100%';
+}
