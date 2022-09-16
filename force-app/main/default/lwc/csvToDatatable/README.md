b95e407413b624d15647a72b882584253db5870d
Sebastiano Vierk <s.vierk@hotmail.de>
2022-09-16 23:37:37 +0200

    feat(lwc): add csv to datatable component (#9)
    * docs(lwc): update overall documentation
    
    * feat(lwc): add csv to datatable component
    
    * test(lwc): add csv to datatable unit tests
    
    * docs(lwc): add csv to datatable readme and image

---
+++ force-app/main/default/lwc/csvToDatatable/README.md
@@ -0,0 +1,5 @@
+# CSV To Datatable
+
+A simple parser for UTF-8 encoded, comma separated .csv files.
+
+<img src="../../../../../images/csv-to-datatable.png" alt="csv-to-datatable" width="500"/>
---
+++ force-app/main/default/lwc/csvToDatatable/csvToDatatable.js
@@ -0,0 +1,77 @@
+import { LightningElement, track } from 'lwc';
+
+/**
+ * A simple parser for UTF-8 encoded, comma separated .csv files.
+ * @alias CsvToDatatable
+ * @extends LightningElement
+ * @hideconstructor
+ *
+ * @example
+ * <c-csv-to-datatable></c-csv-to-datatable>
+ */
+export default class CsvToDatatable extends LightningElement {
+  fileName = 'No file uploaded yet.';
+  @track columns = [];
+  @track data = [];
+
+  handleFileUpload(event) {
+    const files = event.detail.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+      this.fileName = file.name;
+
+      this.read(file);
+    }
+  }
+
+  async read(file) {
+    try {
+      const result = await this.load(file);
+      this.parse(result);
+    } catch (e) {
+      this.error = e;
+    }
+  }
+
+  async load(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = () => {
+        reject(reader.error);
+      };
+      reader.readAsText(file);
+    });
+  }
+
+  parse(csv) {
+    const lines = csv.split(/\r\n|\n/);
+
+    const headers = lines[0].split(',');
+
+    const columns = [];
+    headers.forEach((header) => {
+      columns.push({ label: header, fieldName: header });
+    });
+    this.columns = columns;
+
+    const data = [];
+    lines.forEach((line, i) => {
+      if (i === 0) return;
+
+      const obj = {};
+      const currentline = line.split(',');
+
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+
+      data.push(obj);
+    });
+    this.data = data;
+  }
+}
