aab924cca31ee67e21f3fcf58eafcce2761b32a6
Sebastiano Vierk <s.vierk@hotmail.de>
2022-12-11 17:00:30 +0100

    feat(lwc): add open record page flow action (#11)
    * feat(lwc): add open record page flow action
    
    * test(lwc): add test suite for open record page flow action
    
    * docs(readme): add link to open record page flow action
    
    * docs(lwc): add open record page flow action
    
    * docs(lwc): add readme and image for open record page flow action

---
+++ force-app/main/default/lwc/openRecordPageFlowAction/openRecordPageFlowAction.js
@@ -0,0 +1,31 @@
+import { api, LightningElement } from 'lwc';
+
+/**
+ * Component to forward to a record page from flow.
+ * @alias OpenRecordPageFlowAction
+ * @extends LightningElement
+ * @hideconstructor
+ *
+ * @example
+ * <c-open-record-page-flow-action record-id={recordId} target="_blank"></c-open-record-page-flow-action>
+ */
+export default class OpenRecordPageFlowAction extends LightningElement {
+  /**
+   * Record Id of the record page to which the action should forward.
+   * @type {string}
+   * @default ''
+   */
+  @api recordId;
+
+  /**
+   * Open the page in the same '_self' or in a new tab '_blank '.
+   * @type {string}
+   * @default ''
+   */
+  @api target = '_blank';
+
+  connectedCallback() {
+    const completeURL = `${window.location.origin}/${this.recordId}`;
+    window.open(completeURL, this.target);
+  }
+}
