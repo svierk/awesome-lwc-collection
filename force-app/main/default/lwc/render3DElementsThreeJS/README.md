76c641a6be6cfaa5478fba60d9a8cc479da8fdf8
Sebastiano Schwarz <s.vierk@hotmail.de>
2023-10-09 09:52:14 +0200

    feat(lwc): add component for rendering 3d elements using three.js (#68)
    * feat(lib): add three.js as static resource
    
    * feat(img): add crate material gif
    
    * feat(lwc): add component for rendering 3d elements using three.js
    
    * refactor(lwc): update slds design tokens
    
    * test(lwc): add test suite for 3d elements rendering component
    
    * docs(lwc): update docs and add readme file for 3d rendering component
    
    * test(lwc): adjust mocks for 3d elements rendering component
    
    * test(lwc): remove jest-webgl-canvas-mock

---
+++ force-app/main/default/lwc/render3DElementsThreeJS/README.md
@@ -0,0 +1,5 @@
+# Render 3D Elements (three.js)
+
+A simple demo component for rendering 3D elements using three.js.
+
+<img src="../../../../../images/render-3d-elements.png" alt="render-3d-elements" width="500"/>
---
+++ force-app/main/default/lwc/render3DElementsThreeJS/__tests__/render3DElementsThreeJS.test.js
@@ -0,0 +1,44 @@
+import Render3DElementsThreeJS from 'c/render3DElementsThreeJS';
+import { loadScript } from 'lightning/platformResourceLoader';
+import { createElement } from 'lwc';
+
+jest.mock('lightning/platformResourceLoader', () => ({ loadScript: jest.fn() }), {
+  virtual: true
+});
+
+describe('c-render3-d-elements-three-j-s', () => {
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  it('should create and be accessible', async () => {
+    loadScript.mockResolvedValueOnce(
+      new Promise((resolve) => {
+        global.window.THREE = {
+          Scene: jest.fn(),
+          PerspectiveCamera: jest.fn(),
+          WebGLRenderer: jest.fn(),
+          BoxGeometry: jest.fn(),
+          TextureLoader: jest.fn(),
+          MeshBasicMaterial: jest.fn(),
+          Mesh: jest.fn()
+        };
+        resolve();
+      })
+    );
+
+    // given
+    const element = createElement('c-render3-d-elements-three-j-s', {
+      is: Render3DElementsThreeJS
+    });
+
+    // when
+    document.body.appendChild(element);
+
+    // then
+    expect(element).toBeTruthy();
+    await expect(element).toBeAccessible();
+  });
+});
