2e7d3e5c28ea33595d8ab1d1cf0d548869d5f521
Sebastiano Schwarz <s.vierk@hotmail.de>
2026-02-01 17:07:31 +0100

    chore(setup): remove obsolete graphql jest-mocks

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
     '^lightning/actions$': '<rootDir>/force-app/test/jest-mocks/lightning/actions',
     '^lightning/empApi$': '<rootDir>/force-app/test/jest-mocks/lightning/empApi',
     '^lightning/flowSupport$': '<rootDir>/force-app/test/jest-mocks/lightning/flowSupport',
-    '^lightning/graphql$': '<rootDir>/force-app/test/jest-mocks/lightning/graphql',
     '^lightning/messageService$': '<rootDir>/force-app/test/jest-mocks/lightning/messageService',
     '^lightning/modal$': '<rootDir>/force-app/test/jest-mocks/lightning/modal',
     '^lightning/navigation$': '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
